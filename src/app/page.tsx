9355a1df487a8950edeb2f9417c705f6c404ef8c
jparouca <joaopearoucaa@gmail.com>
2024-03-08 23:38:31 -0300

    last games bunsqlite

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,24 @@
 import { LastGames } from "@/components/last-games";
 import { Hero } from "@/components/ui/background-grid";
+import { Separator } from "@/components/ui/separator";
+import { db } from "@/db";
+import { oracle } from "@/db/schema/oracle";
 
-export default function Home() {
+export default async function Home() {
+  const result = await db.select().from(oracle);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <Hero />
+      <Separator />
       <LastGames />
 
+      {result.map((row) => {
+        return (
+          <div key={row.dpm}>
+            <h1>{row.playername}</h1>
+          </div>
+        );
+      })}
     </main>
   );
 }
